295ba2ac8a454fcd2c929b22861382758733bf5f
*Aleksey <*atsukanov89@gmail.com*>
2023-04-24 16:41:20 +0300

    Выполнено задание № 11

--- script.js
+++ script.js
@@ -71,3 +71,16 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 for (let i = 0; i < arr.length - 1; i++) {
     console.log(arr[i] + arr[i + 1]);
 };
+
+// Задание №11
+
+function squaring(arr) {
+const arr1 = [];
+for (let i = 0; i < arr.length; i++) {
+    const elem = arr[i];
+     let result = elem ** 2;
+       arr1.push(result);
+    }
+    return arr1;
+}
+console.log(squaring([5, 15, 25]));
