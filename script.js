75c243da41fc1ce1d783d46b80216c6b69403376
*Aleksey <*atsukanov89@gmail.com*>
2023-04-24 16:38:58 +0300

    Выполнено задание № 10

--- script.js
+++ script.js
@@ -63,3 +63,11 @@ console.log(letters.split('').reverse().join(''));
  const arr = [[1, 2, 3,], [4, 5, 6]];
 
  console.log(arr.flat());
+
+// Задание №10
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < arr.length - 1; i++) {
+    console.log(arr[i] + arr[i + 1]);
+};
